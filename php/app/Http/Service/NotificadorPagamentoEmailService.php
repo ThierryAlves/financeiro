dd28e96e1552d142c20d1c22ca3ae4bbd4912969
Thierry Alves <thierryalves.oliveira21@gmail.com>
2024-02-20 15:45:47 -0300

    Atualizando validação de response do notificador

--- php/app/Http/Service/NotificadorPagamentoEmailService.php
+++ php/app/Http/Service/NotificadorPagamentoEmailService.php
@@ -17,14 +17,14 @@ class NotificadorPagamentoEmailService implements NotificadorPagamento{
         ];
 
         $response = Http::post(
-            'https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc',
+            'https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6',
             $postBody
         );
 
         $response = json_decode($response->body());
 
-        if ($response->message !== 'Autorizado') {
-            throw new DomainException("Transação não autorizada.");
+        if ($response->message !== true) {
+            throw new DomainException("Email de notificação não enviado.");
         }
     }
 }
