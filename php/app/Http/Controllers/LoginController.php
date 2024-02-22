618fe0cba548f85859953032a909cfebe601c1ef
Thierry Alves <thierryalves.oliveira21@gmail.com>
2024-02-22 13:23:04 -0300

    Adicionando documentação login

--- php/app/Http/Controllers/LoginController.php
+++ php/app/Http/Controllers/LoginController.php
@@ -14,7 +14,93 @@ class LoginController extends Controller
     }
 
     /**
-     * Handle an authentication attempt.
+     * Realiza login no sistema
+     *
+     * @OA\Post(
+     *   path="/api/login/",
+     *   tags={"login"},
+     *   @OA\RequestBody(
+     *       @OA\MediaType(
+     *           mediaType="application/json",
+     *           @OA\Schema(
+     *              @OA\Property(
+     *                   property="email",
+     *                   type="string"
+     *              ),
+     *              @OA\Property(
+     *                   property="senha",
+     *                   type="string"
+     *              ),
+     *              example={"email":"clientepf1@mail.com","senha":"senhaClientePf1"}
+     *           )
+     *       )
+     *     ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Sucesso",
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema(
+     *         @OA\Property(
+     *           property="nome",
+     *           type="string"
+     *         ),
+     *         @OA\Property(
+     *           property="documento",
+     *           type="string"
+     *         ),
+     *         @OA\Property(
+     *           property="email",
+     *           type="string"
+     *         ),
+     *         @OA\Property(
+     *           property="telefone",
+     *           type="string"
+     *         ),
+     *         @OA\Property(
+     *           property="saldo",
+     *           type="integer"
+     *         ),
+     *         @OA\Property(
+     *           property="tipo",
+     *           type="int"
+     *         ),
+     *         @OA\Property(
+     *           property="updated_at",
+     *           type="date"
+     *         ),
+     *         @OA\Property(
+     *           property="created_at",
+     *           type="date"
+     *         ),
+     *         @OA\Property(
+     *           property="id",
+     *           type="int"
+     *         ),
+     *         @OA\Property(
+     *           property="token",
+     *           type="string"
+     *         ),
+     *         example={"id":1,"nome":"Cliente PF 1","documento":"93311421108","email":"clientepf1@mail.com","saldo":1000,"telefone":"6137112480","tipo":1,"deleted_at":null,"created_at":"2024-01-28T15:55:38.000000Z","updated_at":"2024-03-11T15:55:38.000000Z","token":"3c0719d9ebcaf0d2a245549474b70d11"}
+     *       )
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=400,
+     *     description="erro",
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema(
+     *         @OA\Property(
+     *           property="mensagem",
+     *           type="string",
+     *           example="Senha é obrigatório."
+     *         ),
+     *       )
+     *     )
+     *   ),
+     * )
+     *
      */
     public function login(AutenticarRequest $request): Response
     {
